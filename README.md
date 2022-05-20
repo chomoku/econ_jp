1ddd4e65cb502fcc473519ee5faa345fbc1374aa
hide ogawa <mazarimono@gmail.com>
2022-05-20 13:31:25 +0900

    add 3 supermarkets data

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # econ_jp
 
-## 日本の経済指標をデータフレームで扱える
+[![Image from Gyazo](https://i.gyazo.com/7cb486e1e9bf24d184eb9f3e5b0a7696.png)](https://gyazo.com/7cb486e1e9bf24d184eb9f3e5b0a7696)
+## 日本のオープンデータの民主化
 
 econ_jpは日本の経済指標をデータフレームで扱えるPythonのパッケージです。日本の経済指標を前処理無しに使えます。
 
-現在扱えるデータは次の3つです。
+現在扱えるデータは次の8つです。
 
 - 家計調査: 2人以上家計　2000年からの支出額（小分類）
   - 出典：家計調査（家計収支編）　時系列データ（二人以上の世帯）（https://www.stat.go.jp/data/kakei/longtime/index.html）
@@ -15,6 +16,22 @@ econ_jpは日本の経済指標をデータフレームで扱えるPythonのパ�
 - 日銀　マネタリーベース　1970年からの月次推移
   - 出典: 日本銀行　マネタリーベース　https://www.boj.or.jp/statistics/boj/other/mb/index.htm/
   - excel url: https://www.boj.or.jp/statistics/boj/other/mb/mblong.xlsx
+- 日本の各都道府県のコロナ感染者数など
+  - 出典 新型コロナ関連の情報提供:NHK
+  - データURL: "https://www3.nhk.or.jp/n-data/opendata/coronavirus/nhk_news_covid19_prefectures_daily_data.csv"
+- 世界のコロナ感染者数
+  - データソース: Johns Hopkins University https://github.com/CSSEGISandData/COVID-19
+  - データURL: "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
+- 日本のスーパーマーケット数
+  - 統計・データで見るスーパーマーケット:
+    スーパーマーケット店舗数より: http://www.j-sosm.jp/tenpo/index.html
+  - データURL: "http://www.j-sosm.jp/dl/tenpo2204.xlsx"
+- 日本のスーパーマーケットの販売動向データ
+  - 統計・データで見るスーパーマーケット: スーパーマーケット店舗数より: http://www.j-sosm.jp/news/index.html
+  - データURL: "http://www.j-sosm.jp/dl/hanbai_month.xlsx"
+- 日本のスーパーマーケットの各種DI
+  - 統計・データで見るスーパーマーケット: スーパーマーケット店舗数より: http://www.j-sosm.jp/news/index.html
+  - データURL: "http://www.j-sosm.jp/dl/keieidoukou.xlsx"
 
 ## 使い方
 
@@ -43,3 +60,41 @@ from econ_jp import econ_jp
 df = econ_jp.boj_monetary_base()
 ```
 
+- 日本の各都道府県のコロナ感染者数など
+
+```
+from_econ_jp import econ_jp
+df = econ_jp.jp_covid_daily_data()
+```
+
+- 世界のコロナ感染者数
+
+```
+from_econ_jp import econ_jp
+df = econ_jp.global_covid_daily_data()
+```
+
+- 日本のスーパーマーケット数
+
+```
+from_econ_jp import econ_jp
+df = econ_jp.supermarkets_num()
+```
+
+- 日本のスーパーマーケットの販売動向データ
+
+```
+from_econ_jp import econ_jp
+df = econ_jp.supermarkets_sales()
+```
+
+- 日本のスーパーマーケットの各種DI
+
+```
+from_econ_jp import econ_jp
+df = econ_jp.supermarkets_di()
+```
+
+## サンプルノートブック
+
+- [jupyter_notebook](https://github.com/chomoku/econ_jp/blob/main/sample/econ_jp_sample.ipynb)
--- econ_jp/econ_jp.py
+++ econ_jp/econ_jp.py
@@ -1,5 +1,6 @@
 import traceback
 from io import BytesIO
+from datetime import timedelta
 
 import numpy as np
 import pandas as pd
@@ -150,30 +151,117 @@ def jp_covid_daily_data() -> pd.DataFrame:
         r = requests.get(csv_url)
         df = pd.read_csv(BytesIO(r.content))
         df["日付"] = pd.to_datetime(df["日付"])
+        df = df.melt(id_vars=["日付", "都道府県コード", "都道府県名"])
         return df
     except:
-        print('データを取得できませんでした。')
+        print("データを取得できませんでした。")
+
 
 def global_covid_daily_data(cumsum: bool = False) -> pd.DataFrame:
-    '''
+    """
     世界のコロナウィルス感染者数のデータ。
     データソース: Johns Hopkins University https://github.com/CSSEGISandData/COVID-19
     Params:
         cumsum: bool
-            累計値を返すかどうか。初期値はFalse
-    
+            日々の感染者数を返すか、累計値を返すか。初期値はFalseで
+            日々の感染者数を返す。
+
     Returns:
         pd.DataFrame
         インデックスに日付、カラムに国名を持つデータを返す
-    '''
-    df = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv')
-    df = df.set_index('Country/Region')
-    df = df.loc[:, '1/22/20':]
+    """
+    data_url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
+    df = pd.read_csv(data_url)
+    df = df.set_index("Country/Region")
+    df = df.loc[:, "1/22/20":]
     df.columns = pd.to_datetime(df.columns)
     df = df.T
     if not cumsum:
         df = df.diff()
+        df = df.dropna(how="all")
         return df
     else:
         return df
 
+
+def supermarkets_num(tenpo_type: str = "合計") -> pd.DataFrame:
+    """
+    日本のスーパーマーケット数のデータを取得する（単月時系列）
+    データソース: 一般社団法人全国スーパーマーケット協会
+    統計・データで見るスーパーマーケット:
+    スーパーマーケット店舗数より: http://www.j-sosm.jp/tenpo/index.html
+
+    Params:
+        tenpo_type: str
+        スーパーマーケットの種類: '合計', '総合', '大型', '中型', '小型'
+    Returns:
+        pd.DataFrame
+    """
+    data_url = "http://www.j-sosm.jp/dl/tenpo2204.xlsx"
+    try:
+        df = pd.read_excel(data_url, header=2, index_col="集計日", sheet_name=tenpo_type)
+        df.index = df.index.map(lambda x: x - timedelta(1))
+        df.index = pd.to_datetime(df.index)
+        df = df.drop(["Unnamed: 0", "Unnamed: 1"], axis=1)
+        return df
+    except:
+        print("データが取得できません")
+
+
+def supermarkets_sales(data_type: str = "全体", kizon_ten: bool = False) -> pd.DataFrame:
+    """
+    日本のスーパーマーケットの販売動向データを取得する（単月時系列）
+    データソース: 一般社団法人全国スーパーマーケット協会
+    統計・データで見るスーパーマーケット:
+    スーパーマーケット店舗数より: http://www.j-sosm.jp/news/index.html
+    Params:
+        data_type: str
+            データの種別: '全体', 'エリア別', '店舗数別'
+        kizon_ten: bool
+            全店データか既存点データか選択する: 初期値 False: 全店
+    Returns:
+        pd.DataFrame
+    """
+    zen_kizon = ["前年同月比（全店）", "前年同月比（既存店）"]
+
+    try:
+        data_url = "http://www.j-sosm.jp/dl/hanbai_month.xlsx"
+        df = pd.read_excel(data_url, header=[2, 3], sheet_name=data_type)
+        idx = pd.IndexSlice
+        data = df.loc[:, idx[zen_kizon[kizon_ten], :]]
+        first_date = str(df.loc[:, idx["実績年月", :]].loc[0].values[0])
+        date_index = pd.date_range(
+            f"{first_date[:4]}-{first_date[4:]}-1", freq="M", periods=len(data)
+        )
+        data_columns = data.columns.get_level_values(1)
+        data.index = date_index
+        data.columns = data_columns
+        return data
+    except:
+        print("データが取得できません")
+
+
+def supermarkets_di(di_type: str = "売上高DI"):
+    """
+    日本のスーパーマーケットの各種DIを取得する（単月時系列）
+    データソース: 一般社団法人全国スーパーマーケット協会
+    統計・データで見るスーパーマーケット:
+    スーパーマーケット店舗数より: http://www.j-sosm.jp/news/index.html
+    Params:
+        di_type: str
+            DIの種類の指定: '売上高DI', '収益DI', '生鮮品仕入原価DI', '食品仕入原価DI', '販売価格DI', ' 客単価DI', ' 来客数DI'
+    Returns:
+        pd.DataFrame
+    """
+    try:
+        data_url = "http://www.j-sosm.jp/dl/keieidoukou.xlsx"
+        df = pd.read_excel(data_url, header=3, sheet_name=di_type)
+        first_date = str(df["実績年月"][0])
+        index_date = pd.date_range(
+            f"{first_date[:4]}/{first_date[4:]}/1", freq="M", periods=len(df)
+        )
+        df.index = index_date
+        df = df.drop("実績年月", axis=1)
+        return df
+    except:
+        print("データが取得できません")
